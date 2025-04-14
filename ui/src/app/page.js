2d53c4e9cf17640e90d5eb136c3be0c98bd91409
AbdallaMon <abdotlos60@gmail.com>
2025-04-14 16:34:20 +0200

    Fix build issue

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -1,11 +1,12 @@
 import ClientPage from "@/app/UiComponents/client-page/ClientPage.jsx";
 import LanguageProvider from "@/app/providers/LanguageProvider.jsx";
 
-export default function page(){
-    return(
+export default function page() {
+  return (
     <LanguageProvider>
-    <ClientPage/>
+      <Suspense>
+        <ClientPage />
+      </Suspense>
     </LanguageProvider>
-    )
-
+  );
 }
