3a8fef8a2d4e38476372f9b3ece7ac43b2ebb522
AbdallaMon <abdotlos60@gmail.com>
2025-07-26 05:37:07 +0300

    Fix provider

--- ui/src/app/providers/UploadingProgressProvider.js
+++ ui/src/app/providers/UploadingProgressProvider.js
@@ -8,40 +8,43 @@ export default function UploadingProvider({ children }) {
 
   return (
     <UploadingContext.Provider value={{ progress, setProgress, setOverlay }}>
-      {overLay && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            zIndex: 99999999,
-            width: "100vw",
-            height: "100vh",
-            backgroundColor: "rgba(0,0,0,0.6)",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "#fff",
-            fontSize: "20px",
-            flexDirection: "column",
-          }}
-        >
+      <>
+        {overLay && (
           <div
-            style={{ width: "80%", backgroundColor: "#444", borderRadius: 5 }}
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              zIndex: 99999999,
+              width: "100vw",
+              height: "100vh",
+              backgroundColor: "rgba(0,0,0,0.6)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#fff",
+              fontSize: "20px",
+              flexDirection: "column",
+            }}
           >
             <div
-              style={{
-                height: "10px",
-                width: `${progress}%`,
-                backgroundColor: "#4ade80", // green
-                borderRadius: 5,
-                transition: "width 0.2s ease",
-              }}
-            ></div>
+              style={{ width: "80%", backgroundColor: "#444", borderRadius: 5 }}
+            >
+              <div
+                style={{
+                  height: "10px",
+                  width: `${progress}%`,
+                  backgroundColor: "#4ade80", // green
+                  borderRadius: 5,
+                  transition: "width 0.2s ease",
+                }}
+              ></div>
+            </div>
+            <p style={{ marginTop: 10 }}>{progress}% uploaded</p>
           </div>
-          <p style={{ marginTop: 10 }}>{progress}% uploaded</p>
-        </div>
-      )}
+        )}
+        {children}
+      </>
     </UploadingContext.Provider>
   );
 }
