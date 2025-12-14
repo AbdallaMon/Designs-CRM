359d51c54defc42753528c91eaa5f8580f6c75ca
AbdallaMon <abdotlos60@gmail.com>
2025-12-14 21:03:00 +0200

    allowed origin

--- ui/src/app/(auth)/dashboard/(dashboard)/@staff/chat/page.jsx
+++ ui/src/app/(auth)/dashboard/(dashboard)/@staff/chat/page.jsx
@@ -1,7 +1,9 @@
 import ChatPage from "@/app/UiComponents/DataViewer/chat/ChatPage";
+import { connection } from "next/server";
 import { Suspense } from "react";
 
-export default function page() {
+export default async function page() {
+  await connection();
   return (
     <Suspense fallback={<div>Loading Chat...</div>}>
       <ChatPage />;
