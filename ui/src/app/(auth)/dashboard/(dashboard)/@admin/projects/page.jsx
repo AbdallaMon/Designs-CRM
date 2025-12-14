5643002d645d8d54402f78e4449e11b6ebe35ecb
AbdallaMon <abdotlos60@gmail.com>
2025-12-14 20:57:40 +0200

    fix use searchparams

--- ui/src/app/(auth)/dashboard/(dashboard)/@admin/projects/page.jsx
+++ ui/src/app/(auth)/dashboard/(dashboard)/@admin/projects/page.jsx
@@ -1,6 +1,11 @@
 import FinalizedLeadKanban from "@/app/UiComponents/DataViewer/Kanban/staff/FinalizedLeadKanban";
+import { Suspense } from "react";
 
 export default async function page(props) {
   const searchParams = await props.searchParams;
-  return <FinalizedLeadKanban staffId={searchParams.staffId} />;
+  return (
+    <Suspense>
+      <FinalizedLeadKanban staffId={searchParams.staffId} />;
+    </Suspense>
+  );
 }
